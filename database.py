382e1872b80668a4c794ba0f8c7d3a77e33b7e61
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-03 22:39:51 +0530

    Update database.py

--- database.py
+++ database.py
@@ -29,3 +29,33 @@ async def stalking(_: int):
     if stalking:
         return True
     return False
+
+stalkdb = db.stalk
+
+async def add_sudo(_: int):
+    stalking = stalksudo.find_one({"_": _})
+    if stalking:
+        return 
+    return await stalksudo.insert_one({"_": _})
+
+async def del_sudo(_: int):
+    stalking = stalksudo.find_one({"_": _})
+    if not stalking:
+        return 
+    return await stalksudo.delete_one({"_": _})
+
+async def list_sudo():
+    get = stalksudo.find({"_": {"$gt": 0}})
+    if not get:
+        return []
+    lel = await get.to_list(length=10000000000)
+    l = []
+    for l in lel:
+        l.append(lel["_"])
+    return l
+
+async def is_sudo(_: int):
+    stalking = stalksudo.find_one({"_": _})
+    if stalking:
+        return True
+    return False
