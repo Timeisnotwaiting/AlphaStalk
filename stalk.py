496a381d547229e6c4f9308dbb1ef1ccd7df5fe5
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-03 22:37:10 +0530

    Update stalk.py

--- stalk.py
+++ stalk.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters, idle
 from pyrogram.types import Message
 from os import getenv
+from database import *
 
 API_ID = getenv("API_ID")
 API_HASH = getenv("API_HASH")
@@ -9,3 +10,4 @@ OWNER_ID = getenv("OWNER_ID")
 
 app = (":Yashvi:", API_ID, API_HASH, BOT_TOKEN)
 
+@app.on_message(filters.command("stalk"))
