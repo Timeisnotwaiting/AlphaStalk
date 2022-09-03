23aeb16b36b5377f405a757743c415bf6425f720
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-03 22:22:16 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,22 @@
+from mongo import db
+
+stalkdb = db.stalk
+
+async def stalk(_: int):
+    stalking = stalkdb.find_one({"_": _})
+    if stalking:
+        return 
+    return await stalkdb.insert_one({"_": _})
+
+async def unstalk(_: int):
+    stalking = stalkdb.find_one({"_": _})
+    if not stalking:
+        return 
+    return await stalkdb.delete_one({"_": _})
+
+async def list_stalk():
+    get = stalkdb.find({"_": {"$gt": 0}})
+    if not get:
+        return []
+    lel = await get.to_list(length=10000000000)
+    return lel
